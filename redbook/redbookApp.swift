932ca289b866803d1e22f099e1a7c96c8dd927e2
GnaixEuy <sgghcj@live.com>
2022-09-20 12:28:45 +0800

    Initial Commit

---
+++ redbook/redbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  redbookApp.swift
+//  redbook
+//
+//  Created by 苏粤翔 on 2022/9/20.
+//
+
+import SwiftUI
+
+@main
+struct redbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
